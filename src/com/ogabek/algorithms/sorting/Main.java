aed8dc374df84eb44bc2f2652b148f5c0e9b4ed1
OgabekYuldashov <ogabek.yuldashov@gmail.com>
2021-05-01 23:37:15 -0400

    Quick sort implemented

--- src/com/ogabek/algorithms/sorting/Main.java
+++ src/com/ogabek/algorithms/sorting/Main.java
@@ -4,7 +4,14 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Merge Sort:");
         SortingAlgorithm mergeSort = new MergeSort();
         System.out.println(Arrays.toString(mergeSort.sort(new int[]{1, 9, 8, 3, 5, 6, 7, 4, 2})));
+
+        System.out.println("\nQuick Sort:");
+        SortingAlgorithm quickSort = new QuickSort();
+        System.out.println(Arrays.toString(quickSort.sort(new int[]{1, 9, 8, 3, 5, 6, 7, 4, 2})));
+        System.out.println(Arrays.toString(quickSort.sort(new int[]{99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0})));
+
     }
 }
---
+++ src/com/ogabek/algorithms/sorting/QuickSort.java
@@ -0,0 +1,80 @@
+package com.ogabek.algorithms.sorting;
+
+public class QuickSort implements SortingAlgorithm {
+
+    @Override
+    public int[] sort(int[] input) {
+        return inPlaceQuickSort(input, 0, input.length - 1);
+    }
+
+    private int[] inPlaceQuickSort(int[] input, int leftIndex, int rightIndex) {
+        if(rightIndex < leftIndex) return input;
+
+        int pivotIndex = pickPivot(input, leftIndex, rightIndex); // pick a pivot
+
+        swap(input, pivotIndex, rightIndex); // move the pivot to the rightmost index
+
+        int pivotValue = input[rightIndex]; // get the value of pivot which is now at the end
+
+        int newPivotIndex = inPlacePartition(input, pivotValue, leftIndex, rightIndex);
+
+        inPlaceQuickSort(input, leftIndex, newPivotIndex - 1);
+        inPlaceQuickSort(input, newPivotIndex + 1, rightIndex);
+
+        return input;
+    }
+
+
+    /**
+     * partitions the input array into "LessThan", "EqualTo", and "GreaterThan"
+     * sections in comparison to pivot value
+     * @return the new index of the pivot after partitioning
+     */
+    private int inPlacePartition(int[] input, int pivotValue, int leftIndex, int rightIndex) {
+        int partitionIndex = leftIndex;
+
+        for(int i = leftIndex; i < rightIndex; i++) {
+            if(input[i] < pivotValue) {
+                swap(input, i, partitionIndex);
+                partitionIndex++;
+            }
+        }
+        swap(input, rightIndex, partitionIndex);
+        return partitionIndex;
+    }
+
+    private void swap(int[] input, int firstIndex, int secondIndex) {
+        int temp = input[firstIndex];
+        input[firstIndex] = input[secondIndex];
+        input[secondIndex] = temp;
+    }
+
+    private int pickPivot(int[] input, int leftIndex, int rightIndex) {
+        int leftMost = input[leftIndex];
+        int rightMost = input[rightIndex];
+        int midIndex = (leftIndex + rightIndex) / 2;
+        int middle = input[midIndex];
+
+        int medianIndex;
+        if (leftMost > rightMost && leftMost > middle) { // leftmost is the greatest
+            if(rightMost > middle) { // second greatest should be the median
+                medianIndex = rightIndex;
+            } else {
+                medianIndex = midIndex;
+            }
+        } else if (rightMost > middle && rightMost > leftMost){ // rightmost is the greatest
+            if (leftMost > middle) { // second greatest should be the median
+                medianIndex = leftIndex;
+            } else {
+                medianIndex = midIndex;
+            }
+        } else { // middle is the greatest
+            if (leftMost > rightMost) {
+                medianIndex = leftIndex;
+            } else {
+                medianIndex = rightIndex;
+            }
+        }
+        return medianIndex;
+    }
+}
