35d9c4d2fe4374f796f2a38fb58adfe48b1ac521
OgabekYuldashov <ogabek.yuldashov@gmail.com>
2021-05-12 21:37:16 -0400

    Grid traveller tabulation done

--- src/com/ogabek/algorithms/dynamic_programming/GridTraveller.java
+++ src/com/ogabek/algorithms/dynamic_programming/GridTraveller.java
@@ -12,22 +12,49 @@ import java.util.Map;
 public class GridTraveller {
 
     public static void main(String[] args) {
-        System.out.println(travelMemoized(2,3));
-        System.out.println(travelMemoized(18,10));
+        System.out.println(travelMemoizedWithMap(2,3));
+
+        long mapMemoStartTime = System.nanoTime();
+        System.out.println(travelMemoizedWithMap(18,10));
+        long mapMemoEndTime = System.nanoTime();
+        System.out.println("HashMap Memo Execution Time: " + (mapMemoEndTime - mapMemoStartTime));
+
+        long arrayMemoStartTime = System.nanoTime();
+        System.out.println(travelTabulation(18, 10));
+        long arrayMemoEndTime = System.nanoTime();
+        System.out.println("Tabulation Execution Time: " + (arrayMemoEndTime - arrayMemoStartTime));
     }
 
-    public static int travelMemoized(int m, int n) {
+    public static int travelMemoizedWithMap(int m, int n) {
         Map<String, Integer> memo = new HashMap<>();
-        return travel(m, n, memo);
+        return travelMemoizedWithMap(m, n, memo);
     }
 
-    private static int travel(int m, int n, Map<String, Integer> memo) {
+    private static int travelMemoizedWithMap(int m, int n, Map<String, Integer> memo) {
         if(m == 0 || n == 0) return 0;
         if(m == 1 && n == 1) return 1;
         String key = m + "," + n;
-        if(memo.containsKey(key)) return memo.get(key);
+        Integer value = memo.get(key);
+        if(value != null) return value;
+        value = travelMemoizedWithMap(m-1, n, memo) + travelMemoizedWithMap(m, n-1, memo);
+        memo.put(key, value);
+        return value;
+    }
 
-        memo.put(key, travel(m-1, n, memo) + travel(m, n-1, memo));
-        return memo.get(key);
+
+    public static int travelTabulation(int m, int n) {
+        if(m == 0 || n == 0) return 0;
+        if(m == 1 && n == 1) return 1;
+        int[][] table = new int[m+1][n+1];
+        table[1][1] = 1;
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                // if we're within bounds, increment right cell by current value
+                if(j+1 <= n) table[i][j+1] += table[i][j];
+                // if we're within bounds, increment down cell by current value
+                if(i+1 <= m) table[i+1][j] += table[i][j];
+            }
+        }
+        return table[m][n];
     }
 }
