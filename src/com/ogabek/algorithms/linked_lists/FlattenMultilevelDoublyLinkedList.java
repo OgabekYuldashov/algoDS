c4f0bbf5734c6869bdf0d0e58fb2d39f249a658a
OgabekYuldashov <ogabek.yuldashov@gmail.com>
2021-05-22 23:56:19 -0400

    Flatten Multilevel Doubly LinkedList iterative solution done

--- src/com/ogabek/algorithms/linked_lists/FlattenMultilevelDoublyLinkedList.java
+++ src/com/ogabek/algorithms/linked_lists/FlattenMultilevelDoublyLinkedList.java
@@ -2,16 +2,40 @@ package com.ogabek.algorithms.linked_lists;
 
 public class FlattenMultilevelDoublyLinkedList {
     public static void main(String[] args) {
+        Node list1 = new Node(1);
+        list1.addToList(new Node(2));
+        list1.addToList(new Node(3));
+        Node node4 = new Node(4);
+        list1.addToList(node4);
 
+        Node list2 = new Node(5);
+        Node node6 = new Node(6);
+        list2.addToList(node6);
+
+        Node list3 = new Node(7);
+        list3.child = new Node(8);
+
+        list1.child = list2;
+        node4.child = list3;
+
+        Node flatListHead = flatListRecursive(list1);
+        flatListHead.printAll();
     }
 
-    public Node flatten(Node head) {
-        flattenRecursively(head);
+    /**
+     * n -> total number of nodes in the list including the sublist nodes
+     * time: O(n)
+     * space: O(n) when each list consists of one node, and each of these nodes contains a child,
+     * the list becomes vertical. Since recursive solution is a bottom up approach, the call stack
+     * size will be "n", and the worst case occurs
+     */
+    public static Node flatListRecursive(Node head) {
+        recurse(head);
         return head;
     }
 
     // flattens and returns the tail node of a given list
-    private Node flattenRecursively(Node node) {
+    private static Node recurse(Node node) {
         Node prev = node;
         while(node != null) {
             prev = node;
@@ -28,7 +52,7 @@ public class FlattenMultilevelDoublyLinkedList {
             node.child.prev = node;
 
             // 3. get the tail of the child list
-            Node tail = flattenRecursively(node.child);
+            Node tail = recurse(node.child);
             node.child = null; // child list has been flattened, remove the child reference
 
             // 4. adjust the tail references
@@ -45,10 +69,71 @@ public class FlattenMultilevelDoublyLinkedList {
         return prev;
     }
 
+    /**
+     * time: O(n) -> more iterations than "n" since we might iterate over some newly merged child
+     * nodes again on the parent level
+     *
+     * space: O(1)
+     */
+    public static Node flatListIterative(Node head) {
+        Node current = head;
+        while(current != null) {
+            // 1. skip if there's no child
+            if(current.child == null) {
+                current = current.next;
+                continue;
+            }
+
+            // 2. traverse to the tail of the child list
+            Node next = current.next; // next node in the parent list
+            Node childTail = current.child; // child tail
+            while(childTail.next != null) {
+                childTail = childTail.next;
+            }
+
+            // 3. merge the child tail to the original list
+            childTail.next = next;
+            if(next != null) {
+                next.prev = childTail;
+            }
+
+            // 4. merge the child head to the original list
+            current.next = current.child;
+            current.child.prev = current;
+            current.child = null;
+
+            current = current.next;
+        }
+
+        return head;
+    }
+
     static class Node {
         public int val;
         public Node prev;
         public Node next;
         public Node child;
+
+        public Node(int val) {
+            this.val = val;
+        }
+
+        public void addToList(Node node) {
+            Node tail = this;
+            while (tail.next != null) {
+                tail = tail.next;
+            }
+            tail.next = node;
+            node.prev = tail;
+        }
+
+        public void printAll() {
+            System.out.print(val + "<->");
+            if(next != null) {
+                next.printAll();
+            } else {
+                System.out.print("null\n");
+            }
+        }
     }
 }
