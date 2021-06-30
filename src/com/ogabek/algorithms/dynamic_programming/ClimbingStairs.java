47ea10c0f3144daf882403594b72dddddd20ae74
Ogabek Yuldashov <ogabek.yuldashov@gmail.com>
2021-06-30 19:28:33 -0400

    Slight optimization for Climbing Stairs Tabulation solution

--- src/com/ogabek/algorithms/dynamic_programming/ClimbingStairs.java
+++ src/com/ogabek/algorithms/dynamic_programming/ClimbingStairs.java
@@ -32,7 +32,7 @@ public class ClimbingStairs {
         int n = cost.length;
 
         for(int i = 2; i < n; i++) {
-            cost[i] = Math.min(cost[i-1] + cost[i], cost[i-2] + cost[i]);
+            cost[i] = cost[i] + Math.min(cost[i-1], cost[i-2]);
         }
 
         return Math.min(cost[n-1], cost[n-2]);
