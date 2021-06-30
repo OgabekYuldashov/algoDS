6e5ba9e74d311b64167670bb301e1bcc4dbec2bf
Ogabek Yuldashov <ogabek.yuldashov@gmail.com>
2021-06-30 19:12:02 -0400

    Climbing Stairs Memoization and Tabulation solutions done

---
+++ src/com/ogabek/algorithms/dynamic_programming/ClimbingStairs.java
@@ -0,0 +1,40 @@
+package com.ogabek.algorithms.dynamic_programming;
+
+public class ClimbingStairs {
+    Integer[] memo;
+
+    /**
+     * time: O(n)
+     * space: O(n)
+     */
+    public int minCostClimbingStairsMemoization(int[] cost) {
+        int n = cost.length;
+        memo = new Integer[n];
+
+        return Math.min(minCost(n - 1, cost), minCost(n - 2, cost));
+    }
+
+    public int minCost(int pos, int[] cost) {
+        if(pos < 0) return 0;
+        if(pos == 0 || pos == 1) return cost[pos];
+        if(memo[pos] != null) return memo[pos];
+
+        memo[pos] = cost[pos] + Math.min(minCost(pos - 1, cost), minCost(pos - 2, cost));
+        return memo[pos];
+    }
+
+
+    /**
+     * time: O(n)
+     * space: O(1)
+     */
+    public int minCostClimbingStairsTabulation(int[] cost) {
+        int n = cost.length;
+
+        for(int i = 2; i < n; i++) {
+            cost[i] = Math.min(cost[i-1] + cost[i], cost[i-2] + cost[i]);
+        }
+
+        return Math.min(cost[n-1], cost[n-2]);
+    }
+}
